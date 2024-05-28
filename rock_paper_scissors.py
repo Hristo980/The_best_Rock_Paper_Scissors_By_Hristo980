28d05566ca7a9546f24f69eaa847c2a8cfaecbdb
Hristo980 <hristooo96@gmail.com>
2024-05-28 08:38:38 +0300

    Updated rock_paper_scissors.py

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -6,6 +6,8 @@ paper = 'Paper'
 scissors = 'Scissors'
 wins_counter = 0
 lose_counter = 0
+wins_counter_for_current_result = 0
+lose_counter_for_current_result = 0
 
 while wins_counter < 2 or lose_counter < 2:
 
@@ -30,36 +32,44 @@ while wins_counter < 2 or lose_counter < 2:
     elif computer_random_number == 3:
         computer_random_move = scissors
 
-    print(Fore.BLUE + f'The computer chose {computer_random_move}.')
-
     if (player_move == rock and computer_random_move == scissors) or \
             (player_move == paper and computer_random_move == rock) or \
             (player_move == scissors and computer_random_move == paper):
-        print(Fore.GREEN + 'You win!')
+        wins_counter_for_current_result += 1
+        print(Fore.GREEN + f'The computer chose {computer_random_move} and your {player_move} beats it! You win!')
+        print(Fore.GREEN + f'Current result {wins_counter_for_current_result}:{lose_counter_for_current_result}')
         wins_counter += 1
         if wins_counter >= 2:
+            wins_counter_for_current_result = 0
+            lose_counter_for_current_result = 0
             wins_counter = 0
-            print(Fore.GREEN + 'Final result: You are the big winner!')
-            one_more_game = input(Fore.CYAN + 'Type [y] to Play Again or [n] to quit: ')
+            lose_counter = 0
+            print(Fore.GREEN + 'Final result: You are the big winner! :)')
+            one_more_game = input(Fore.BLACK + 'Type [y] to Play Again or [n] to quit: ')
             if one_more_game.lower() == 'y':
-                wins_counter = 0
                 continue
             elif one_more_game.lower() == 'n':
-                print(Fore.CYAN + 'Thank you for playing with us!')
+                print(Fore.BLACK + 'Thank you for playing with us, you smashed the computer!')
                 break
     elif player_move == computer_random_move:
-        print(Fore.YELLOW + 'Draw!')
-
+        wins_counter_for_current_result += 1
+        lose_counter_for_current_result += 1
+        print(Fore.YELLOW + f'The computer chose {computer_random_move} which is equal to your {player_move}! Draw!')
+        print(Fore.YELLOW + f'Current result {wins_counter_for_current_result}:{lose_counter_for_current_result}')
     else:
-        print(Fore.RED + 'You lose!')
+        lose_counter_for_current_result += 1
+        print(Fore.RED + f'The computer chose {computer_random_move} which beats your {player_move}! You lose!')
+        print(Fore.RED + f'Current result {wins_counter_for_current_result}:{lose_counter_for_current_result}')
         lose_counter += 1
         if lose_counter >= 2:
+            wins_counter_for_current_result = 0
+            lose_counter_for_current_result = 0
+            wins_counter = 0
             lose_counter = 0
-            print(Fore.RED + 'Final result: The computer is the big winner!')
-            one_more_game = input(Fore.CYAN + 'Type [y] to Play Again or [n] to quit: ')
+            print(Fore.RED + 'Final result: The computer is the big winner! :(')
+            one_more_game = input(Fore.BLACK + 'Type [y] to Play Again or [n] to quit: ')
             if one_more_game.lower() == 'y':
-                lose_counter = 0
                 continue
             elif one_more_game.lower() == 'n':
-                print(Fore.CYAN + 'Thank you for playing with us!')
+                print(Fore.BLACK + "Thank you for playing with us and please don't give up, it's just a game!")
                 break
